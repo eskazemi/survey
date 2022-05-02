5d2a772e3604e76a193716be008541c87b0234e4
eskazemi <smk182018@gmail.com>
2022-05-03 00:12:15 +0430

    fix title

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from app.api.api import api_router
 from mongoengine import connect
 
-connect(db="first", port=27017)
+connect(db="survey", port=27017)
 
-app = FastAPI(title="python_poll", version="0.0.1")
+app = FastAPI(title="survey", version="0.0.1")
 
 app.include_router(api_router)
 
